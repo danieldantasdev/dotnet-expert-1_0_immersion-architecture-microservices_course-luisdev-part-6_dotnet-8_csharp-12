14fd6be569dfc301d6d1fb22e58669e076acc465
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:36:40 -0300

    feat: adição do arquivo IGetOrderByIdUseCase.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-ok/src/AwesomeShop.Services.Orders.Application/UseCases/GetOrderById/IGetOrderByIdUseCase.cs
@@ -0,0 +1,7 @@
+namespace AwesomeShop.Services.Orders.Application.UseCases.GetOrderById
+{
+    public interface IGetOrderByIdUseCase
+    {
+        Task<GetOrderByIdOutput> Execute(Guid id);
+    }
+}
