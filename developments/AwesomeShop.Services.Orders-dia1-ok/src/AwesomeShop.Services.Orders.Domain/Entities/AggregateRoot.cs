5083b6d7bd811c179fed6fd697f1d3709f9570e3
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:36:31 -0300

    feat: adição do arquivo AggregateRoot.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-ok/src/AwesomeShop.Services.Orders.Domain/Entities/AggregateRoot.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AwesomeShop.Services.Orders.Domain.Events;
+
+namespace AwesomeShop.Services.Orders.Domain.Entities
+{
+    public abstract class AggregateRoot : IEntityBase
+    {
+        private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
+        public Guid Id { get; protected set; }
+        public IEnumerable<IDomainEvent> Events => _events;
+
+        protected void AddEvent(IDomainEvent @event) {
+            _events.Add(@event);
+        }
+    }
+}
