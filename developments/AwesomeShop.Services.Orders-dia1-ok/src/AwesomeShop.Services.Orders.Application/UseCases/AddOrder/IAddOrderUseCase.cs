e80b593504b973d175a77cf781af3265ac5ca1dc
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:36:39 -0300

    feat: adição do arquivo IAddOrderUseCase.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-ok/src/AwesomeShop.Services.Orders.Application/UseCases/AddOrder/IAddOrderUseCase.cs
@@ -0,0 +1,7 @@
+namespace AwesomeShop.Services.Orders.Application.UseCases.AddOrder
+{
+    public interface IAddOrderUseCase
+    {
+        Task<AddOrderOutput> Execute(AddOrderInput input);
+    }
+}
