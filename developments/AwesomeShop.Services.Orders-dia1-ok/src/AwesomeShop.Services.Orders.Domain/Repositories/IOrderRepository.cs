1658f8fe598bec8d73d7bb26bc357ffa3571f087
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:36:35 -0300

    feat: adição do arquivo IOrderRepository.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-ok/src/AwesomeShop.Services.Orders.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,11 @@
+using AwesomeShop.Services.Orders.Domain.Entities;
+
+namespace AwesomeShop.Services.Orders.Domain.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<Order> GetByIdAsync(Guid id);
+        Task AddAsync(Order order);
+        Task UpdateAsync(Order order);
+    }
+}
