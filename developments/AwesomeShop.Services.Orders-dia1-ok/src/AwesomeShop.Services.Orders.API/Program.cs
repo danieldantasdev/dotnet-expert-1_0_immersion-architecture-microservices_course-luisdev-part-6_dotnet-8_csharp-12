0c70e379db0e141dfe060c71c548790313733c63
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:36:25 -0300

    feat: adição do arquivo Program.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-ok/src/AwesomeShop.Services.Orders.API/Program.cs
@@ -0,0 +1,36 @@
+using AwesomeShop.Services.Orders.Application;
+using AwesomeShop.Services.Orders.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
+
+// Add services to the container.
+builder.Services
+
+    .AddInfrastructure()
+    .AddApplication();
+
+    
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
