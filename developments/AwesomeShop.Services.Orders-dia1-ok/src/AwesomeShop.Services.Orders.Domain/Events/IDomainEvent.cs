1053b769fa1769d9dc528e4c448e5a1607c500ab
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:36:37 -0300

    feat: adição do arquivo IDomainEvent.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-ok/src/AwesomeShop.Services.Orders.Domain/Events/IDomainEvent.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeShop.Services.Orders.Domain.Events
+{
+    public interface IDomainEvent
+    {
+        
+    }
+}
