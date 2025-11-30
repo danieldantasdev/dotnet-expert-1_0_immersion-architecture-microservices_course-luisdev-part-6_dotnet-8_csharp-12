853f410b2c2af8b0f5d008e8ffbd4aa94c398ad5
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:36:38 -0300

    feat: adição do arquivo AddOrderUseCase.cs

---
+++ developments/AwesomeShop.Services.Orders-dia1-ok/src/AwesomeShop.Services.Orders.Application/UseCases/AddOrder/AddOrderUseCase.cs
@@ -0,0 +1,27 @@
+using AwesomeShop.Services.Orders.Domain.Repositories;
+using AwesomeShop.Services.Orders.Infrastructure.MessageBus;
+
+namespace AwesomeShop.Services.Orders.Application.UseCases.AddOrder
+{
+    public class AddOrderUseCase : IAddOrderUseCase
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IEventProcessor _eventProcessor;
+        public AddOrderUseCase(IOrderRepository orderRepository, IEventProcessor eventProcessor)
+        {
+            _orderRepository = orderRepository;
+            _eventProcessor = eventProcessor;
+        }
+
+        public async Task<AddOrderOutput> Execute(AddOrderInput input)
+        {
+            var order = input.ToEntity();
+
+            await _orderRepository.AddAsync(order);
+
+            _eventProcessor.Process(order.Events);
+            
+            return new AddOrderOutput(order.Id);
+        }
+    }
+}
